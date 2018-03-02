f0255d0f9ea0c555fa23077c42cf2b32e7755366
Marcel Grama <aace375@yahoo.ro>
2018-03-02 22:49:55 +0200

    S

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-var note = [4, 4.5, 8, 10, 10, 10, 5, 7];
-var elevi = ['Dan', 'Gelu', 'Gabriel', 'Sonia', 'Larisa', 'Maria', 'Sergiu', 'Bogdan'];
+var grades = [4, 4.5, 8, 10, 10, 10, 5, 7];
+var students = ['Dan', 'Gelu', 'Gabriel', 'Sonia', 'Larisa', 'Maria', 'Sergiu', 'Bogdan'];
 
-function afiseaza(text) {
-    var rezElm = document.getElementById('rezultat');
-    rezElm.textContent += JSON.stringify(text, null, 6);
+function showIt(text) {
+    var resElm = document.getElementById('resultat');
+    resElm.textContent += JSON.stringify(text, null, 6);
 }
 
 function concat() {
     var total = 0;
     var length = 0;
-    var rez = {};
+    var res = {};
     var key;
     var val;
     
-    for (var i=0; i<note.length;i++) {
-        key = elevi[i]; //la fiecare iterare key ia numele elevului curent
-        val = note[i]; //la fiecare iterare val ia nota curenta
-        //rez[key] = val;
-        rez[key]+=[{ 'nota': val }]; //inserez nota la fiecare elev
+    for (var i=0; i<grades.length;i++) {
+        key = students[i]; 
+        val = grades[i]; 
+        //res[key] = val;
+        res[key]+=[{ 'grade': val }]; 
         
-        if (val >= 4.5) { //daca nota > sau = 4.5
-            rez[key] += [{ 'admis': true }]; //adaug la fiecare elev admis:true
+        if (val >= 4.5) { //if grade > or = 4.5
+            res[key] += [{ 'pass': true }]; //pass:true
 
         }else{
-            rez[key] += [{ 'admis': false }]; //admis:fals
+            res[key] += [{ 'pass': false }]; //pass:fals
         }
-        total += val; //la fiecare iterare adun nota curenta la total
-        length++; //incrementez cu 1 fiecare ciclu sa retin cate note am
+        total += val; // sum the grades
+        length++; //adding +1 at every iteration to find how many grades I have
         }  
-    var average = total / length; //media aritmetica
-    // var media= average.toFixed(2); //convertesc nr. la 2 zecimale
-    rez.media = average.toFixed(2); //adaug proprietatea media cu valoarea mediei aritmetice si convertesc la 2 zecimale
-    return rez; //returnez obiectul
+    var average = total / length; //average of grades
+    // var media= average.toFixed(2); 
+    res.media = average.toFixed(2); 
+    return res; //returnez obiectul
 
 }
-var show = concat(); //apelez functia concat
-afiseaza(show); //apelez functia afiseaza cu parametrul show
+var show = concat();
+showIt(show); 
 
 /*
-Uneste cele doua liste "note" si "elevi" pentru a obtine
+Uneste cele doua liste "grades" si "students" pentru a obtine
 un obiect de forma urmatoare:
-var rez = {
+var res = {
 	Dan: {
-  	nota: 4,
-    admis: false,
+  	grade: 4,
+    pass: false,
   },
   Gelu: {
-  	nota: 4.5,
-    admis: true,
+  	grade: 4.5,
+    pass: true,
   },
   ...
   media: 6.23,
 };
 
-Elevii cu nota mai mare de nota 4.5 (inclusiv) vor avea proprietatea "admis: true"
-iar ceilalti "admis: false".
-Proprietatea "media" va contine media notelor sub forma "x.xx".
-Foloseste functia afiseaza pentru a afisa rezultatul.
+studentsi cu grade mai mare de grade 4.5 (inclusiv) vor avea proprietatea "pass: true"
+iar ceilalti "pass: false".
+Proprietatea "media" va contine media gradeslor sub forma "x.xx".
+Foloseste functia showIt pentru a afisa resultatul.
 */
